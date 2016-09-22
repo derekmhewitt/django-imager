89a8bc81d84d9a3d6c4bda2471253d5491bedacf
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-21 17:44:50 -0700

    updated requirements.txt, added more testing files.  Notes for testing are in imager_images/tests/tests.py

--- imager_images/tests/tests.py
+++ imager_images/tests/tests.py
@@ -10,3 +10,10 @@ class PhotoTestCase(TestCase):
 
     def test_babys_first_test(self):
         assert 1 == 1
+
+"""
+OK, so I had a conversation with Cris about testing.  First of all, I'm going to go after our models with a buzzsaw and cut out just about everything that isn't explicitely required as part of the assignment.  Additional things to do:
+- create factory.py in each tests directory, our factories live there
+- remember the difference between PhotoFactory.create() and .build() <-- this lets us test that properties instantiate properly when we save things manually inside our tests.
+- Test created-on and last-modified dates by comparison - modify an extra property on the instance of the model and assert those dates are different, and that modified is after created.
+- create a different test_modelname file for each model and another diff test file for every other major division."""
