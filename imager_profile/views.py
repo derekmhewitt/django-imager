b37cfeb94b92a7f1be52654d48dc69ac81956ed0
Derek Hewitt <derekmhewitt@gmail.com>
2016-10-05 15:44:03 -0700

    work on imager_profile profile view.  Added individual album and photo templates to imager_images.

--- imager_profile/views.py
+++ imager_profile/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from ..imager_image.models import Photo, Album
 
-# Create your views here.
+
 class ProfileView(TemplateView):
     template_name = 'profile_view.html'
 
     def get_context_data(self, request, **kwargs):
+        current_user = request.user
         context = super(ProfileView, self).get_context_data(**kwargs)
-        
+        context['num_albums'] = len(Album.objects.all().filter(user=current_user))
+        context['five_albums'] = Album.objects.all().filter(user=current_user)[:5]
+        context['num_photos'] = len(Photo.objects.all().filter(user=current_user))
+        context['five_photos'] = Photo.objects.all().filter(user=current_user)[:5]
+        return context
