4560eec98e07c98ac4d7881684cc86c4d9d6a279
Crystal Lessor <clessor88@yahoo.com>
2016-10-05 15:16:49 -0700

    changed profile_view and views.py

--- imager_profile/views.py
+++ imager_profile/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class ProfileView(TemplateView):
+    template_name = 'profile_view.html'
+
+    def get_context_data(self, request, **kwargs):
+        context = super(ProfileView, self).get_context_data(**kwargs)
+        
