03aa18bd782710b23c622ca8a915457bb76952f7
Derek Hewitt <derekmhewitt@gmail.com>
2016-10-06 11:34:23 -0700

    created album create+edit+delete templates, modified formatting in a few templates a little, fixed photo create view w/ Will's help

--- imager_images/urls.py
+++ imager_images/urls.py
@@ -2,19 +2,18 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Photo
+from .models import Photo, Album
+from django.http import request
 
-from .views import PHOTO_FORM_FIELDS, ALBUM_FORM_FIELDS
+from .views import PHOTO_FORM_FIELDS, ALBUM_FORM_FIELDS, PhotoCreate
 
 urlpatterns = [
     url(r'^$',
-        login_required(TemplateView.as_view(template_name="imager_images/library.html")),
+        login_required(TemplateView.as_view(
+            template_name="imager_images/library.html")),
         name='my_library'),
     url(r'^photo/create/$',
-        login_required(CreateView.as_view(
-            template_name="imager_images/photo_create.html",
-            model=Photo,
-            fields=PHOTO_FORM_FIELDS)),
+        login_required(PhotoCreate.as_view()),
         name='photo_create'),
     url(r'^photo/edit/(?P<pk>[0-9]+)/$',
         login_required(UpdateView.as_view(
@@ -27,4 +26,21 @@ urlpatterns = [
             template_name="imager_images/photo_delete.html",
             model=Photo)),
         name='photo_delete'),
+    url(r'^album/create/$',
+        login_required(CreateView.as_view(
+            template_name="imager_images/album_create.html",
+            model=Album,
+            fields=ALBUM_FORM_FIELDS)),
+        name='album_create'),
+    url(r'^album/edit/(?P<pk>[0-9]+)/$',
+        login_required(UpdateView.as_view(
+            template_name="imager_images/album_edit.html",
+            model=Album,
+            fields=ALBUM_FORM_FIELDS)),
+        name='album_edit'),
+    url(r'^album/delete/(?P<pk>[0-9]+)/$',
+        login_required(DeleteView.as_view(
+            template_name="imager_images/album_delete.html",
+            model=Album)),
+        name='album_delete'),
 ]
--- imager_images/views.py
+++ imager_images/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from imager_images.models import Photo, Album
+from django.views.generic.edit import CreateView
 
 PHOTO_FORM_FIELDS = [
     'file',
@@ -42,6 +43,14 @@ class LibraryView(TemplateView):
         context['all_photos'] = Photo.objects.all().filter(user=current_user)
         return context
 
-# class PhotoCreate(CreateView):
-#     model = Photo
-#     fields = ['name']
+
+class PhotoCreate(CreateView):
+    template_name = "imager_images/photo_create.html"
+    model = Photo
+    fields = PHOTO_FORM_FIELDS
+    # success_url = library view
+
+    def form_valid(self, form):
+        # import pdb;pdb.set_trace()
+        form.instance.user = self.request.user
+        return super(PhotoCreate, self).form_valid(form)
