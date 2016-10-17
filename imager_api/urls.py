bf9d4948b620760823d4dbbd70945395d6eda87c
Derek Hewitt <derekmhewitt@gmail.com>
2016-10-17 12:38:26 -0700

    more work on the api.  Updated the serializers to use Hyperlinked model serializers and refactored other code to take that into account

--- imager_api/urls.py
+++ imager_api/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required
-from .views import PhotoViewSet
+from .views import PhotoViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register(r'photos', PhotoViewSet)
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
     url(r'^v1/$', login_required(router.urls)),
-    url(r'^api-auth/$', include('rest_framework.urls',
-                                namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls',
+        namespace='rest_framework'))
 ]
--- imager_api/views.py
+++ imager_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from imager_images.models import Photo
+from rest_framework import viewsets
+from .serializers import UserSerializer, PhotoSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """Automatically provides 'list' and 'detail' views."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permissions_classes = (IsAuthenticatedOrReadOnly,
+                           IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
